93b348db605ac2415df2c83a20ed6dab84642d2d
Matt Sam <MSTTmm1@gmail.com>
2021-08-25 17:42:58 -0400

    。。。

--- training_tools/prep_mark.py
+++ training_tools/prep_mark.py
@@ -15,33 +15,6 @@ if not os.path.isdir(pics_path):
     os.makedirs(pics_path)
 
 
-def prep_box(layerOutputs, frame_width, frame_height):
-    boxes1 = []
-    boxes2 = []
-    confidences1 = []
-    confidences2 = []
-    for output in layerOutputs:
-        for detection in output:
-            scores = detection[5:]
-            classID = np.argmax(scores)
-            confidence = scores[classID]
-            if confidence > 0.1 and classID == 0:
-                box = detection[:4] * np.array([frame_width, frame_height, frame_width, frame_height])
-                (centerX, centerY, width, height) = box.astype("int")
-                box = [int(centerX), int(centerY), int(width), int(height)]
-                if classID == 0:
-                    boxes1.append(box)
-                    confidences1.append(float(confidence))
-                elif classID == 1:
-                    boxes2.append(box)
-                    confidences2.append(float(confidence))
-
-    indices1 = cv2.dnn.NMSBoxes(boxes1, confidences1, 0.4, 0.3)
-    indices2 = cv2.dnn.NMSBoxes(boxes2, confidences2, 0.4, 0.3)
-
-    return indices1, indices2, boxes1, boxes2
-
-
 if __name__ == '__main__':
     if not os.listdir(pics_path):
         exit(0)
@@ -49,12 +22,11 @@ if __name__ == '__main__':
     CONFIG_FILE = './yolov4-tiny.cfg'
     WEIGHT_FILE = './yolov4-tiny.weights'
 
-    net = cv2.dnn.readNetFromDarknet(CONFIG_FILE, WEIGHT_FILE)
+    net = cv2.dnn.readNet(CONFIG_FILE, WEIGHT_FILE)
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-
-    ln = net.getLayerNames()
-    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    model = cv2.dnn_DetectionModel(net)
+    model.setInputParams(size=(512, 320), scale=1/255, swapRB=False)
 
     start_time = time()
 
@@ -62,25 +34,20 @@ if __name__ == '__main__':
         pic_names, pic_ext = os.path.splitext(pics)
         file_create = open(prep_label_path + pic_names + '.txt', 'w')
         pictures = cv2.imdecode(np.fromfile(pics_path + pics, dtype=np.uint8), -1)
+        pictures = pictures[..., :3]
         # cv2.imshow('Show frame', pictures)
         # cv2.waitKey(1)
 
         frame_height, frame_width = pictures.shape[:2]
-        blob = cv2.dnn.blobFromImage(pictures, 1 / 255.0, (288, 512), swapRB=False, crop=False)
-        net.setInput(blob)
-        layerOutputs = net.forward(ln)
+        classes, scores, boxes = model.detect(pictures, 0.2, 0.3)
 
-        indices1, indices2, boxes1, boxes2= prep_box(layerOutputs, frame_width, frame_height)
-        if len(indices1) > 0:
-            for i in indices1.flatten():
-                (x, y) = (boxes1[i][0], boxes1[i][1])
-                (w, h) = (boxes1[i][2], boxes1[i][3])
+        for (classid, score, box) in zip(classes, scores, boxes):
+            x, y, w, h = box
+            x = x + w/2
+            y = y + h/2
+            if classid == 0:
                 file_create.write(f'0 {(x/frame_width):.6f} {(y/frame_height):.6f} {(w/frame_width):.6f} {(h/frame_height):.6f}\n')
-
-        if len(indices2) > 0:
-            for i in indices2.flatten():
-                (x, y) = (boxes2[i][0], boxes2[i][1])
-                (w, h) = (boxes2[i][2], boxes2[i][3])
+            if classid == 1:
                 file_create.write(f'1 {(x/frame_width):.6f} {(y/frame_height):.6f} {(w/frame_width):.6f} {(h/frame_height):.6f}\n')
 
         file_create.close()
